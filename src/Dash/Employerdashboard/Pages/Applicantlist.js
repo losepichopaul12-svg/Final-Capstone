93303696277c74a362b37f996b9027ac2df28637
losepicho paul <losepichopau12@gmail.com>
2026-03-06 05:18:34 +0300

    api changes

--- src/Dash/Employerdashboard/Pages/Applicantlist.js
+++ src/Dash/Employerdashboard/Pages/Applicantlist.js
@@ -33,6 +33,15 @@ function Applicantlist(){
  useEffect(() =>{
   newapplicants()
  },[]);
+//  updating applicant status
+const updateStatus = async (id, status) => {
+
+await axios.put(
+`https://capstonebackend-bh74.onrender.com/status/update-status/${id}`,{ status });
+
+newapplicants();
+
+};
     return(
          <div className="container mt-3">
       <div className="card p-3 shadow-sm">
@@ -42,20 +51,20 @@ function Applicantlist(){
           <thead>
             <tr>
               <th>Applicant Full Names</th>
+              <th>Applicant Email Adress</th>
               <th>Job Title</th>
               <th>Status</th>
             </tr>
           </thead>
           <tbody>
-            {Applications.map((Application,index) => (
-              <tr key={Application.index}>
+            {Applications.map((Application) => (
+              <tr key={Application._id}>
                 <td>{Application.applicantname}</td>
                 <td>{Application.applicantemail}</td>
                <td>{Application.Jobtitle}</td>
                 <td>
-                  <button className="btn btn-sm btn-outline-success">
-                    {Application.status}
-                  </button>
+                  <button className="btn btn-success m-2"onClick={() => updateStatus(Application._id,"Shortlisted")}>Shortlist</button>
+                  <button className="btn btn-danger" onClick={() => updateStatus(Application._id,"Rejected")}>Reject</button>
                 </td>
               </tr>
             ))}
--- src/Dash/Jobseekerdashboard/pages/Findjob.js
+++ src/Dash/Jobseekerdashboard/pages/Findjob.js
@@ -1,8 +1,14 @@
 import { useState,useEffect } from "react";
 import axios from "axios"
+import { Link } from "react-router-dom";
 
 function Findjob(){
- const[Jobs,setJobs]=useState([])
+ const[Jobs,setJobs]=useState([]);
+ const[open, setOpen] = useState(false);
+ const[applicantname,setapplicantname]=useState("");
+ const[applicantemail,setapplicantemail]=useState("");
+ const[selectedJob, setSelectedJob] = useState(null);
+ 
    const newJobs = async () => {
     try {
       const response = await axios.get("https://capstonebackend-bh74.onrender.com/fetch-jobs");
@@ -17,9 +23,32 @@ function Findjob(){
   newJobs()
  },[]);
 
+ const senddetails=async(e)=>{
+  e.preventDefault();
+   console.log("Form submitted");
+  setOpen(false);
+   if(!applicantname || !applicantemail ){
+        alert("Please fill all fields required")
+        return;
+
+    }
+    console.log("Sending application  data to database API ")
+  const response=await axios.post("https://capstonebackend-bh74.onrender.com/api/sendapplication",
+    {
+  jobId: selectedJob._id,
+  Jobtitle:selectedJob.Jobtitle,
+  applicantname:applicantname,
+  applicantemail:applicantemail
+    }
+  )
+  console.log("the response from server is: ", response.data);
+  alert(response.data.message)
+
+ }
+
     return(
       <div className="container mt-2">
-       <h2 className="mb-4 text-center ft-italic text-uppercase fw-bold">My Jobs</h2>
+       <h2 className="mb-4 text-center ft-italic text-uppercase fw-bold">Apply Available Jobs</h2>
   
 
   <div className="row ">
@@ -34,17 +63,65 @@ function Findjob(){
             <h6>Employer  :{Job.Employer}</h6>
             <p className="small mb-1"><strong>Location :</strong>{Job.Location}</p>
             <p className="small mb-1"><strong>Job type:</strong>{Job.Selected}</p>
-            <p className="small mb-1"><strong>Deadline Date :</strong>{new Date(Job.Date).toLocalString}</p>
+            <p className="small mb-1"><strong>Deadline Date :</strong>{Job.Date}</p>
             <p className="fw-bold"><strong>Ksh :</strong>{Job.Salary}</p>
             <p className="small text-muted lh-sm mb-2">{Job.Description}</p>
-            <button  className="btn btn-primary w-100 mt-auto"> Apply Now</button>
-            {/* onClick={applyJob(Job._id)} */}
+            <div>
+            <button className="btn btn-primary w-100 mt-auto"  onClick={() => {  setSelectedJob(Job); setOpen(true)}} > Apply Now</button>
+             
+
+            </div>
           </div>
         </div>
       </div>
     ))}
 
   </div>
+  {open &&  (
+    
+ <div  className="modal custom-modal">
+ <div className="">
+ <div className="modal-content ">
+ <Link to="Application" className="link">view current Application </Link>
+ <div className="header">
+<h2 className="modal-title">Application Form</h2>
+<p>Fill the following form correct details to be considered during shortlisting.</p>
+</div>
+<div className="modal-body">
+ <form onSubmit={senddetails}>
+   <div className="mb-3">
+   <p> Applying for: <strong>{selectedJob?.Jobtitle}</strong> </p>
+              <label className="form-label">Applicant Names</label>
+              <input
+                className="form-control"
+                type="text"
+                value={applicantname}
+                placeholder="Enter your full names"
+                onChange={(e) => setapplicantname(e.target.value)}
+                required
+              />
+            </div>
+             <div className="mb-3">
+              <label className="form-label">Applicant Email</label>
+              <input
+                className="form-control"
+                type="email"
+                value={applicantemail}
+                placeholder="Enter your email"
+                onChange={(e) => setapplicantemail(e.target.value)}
+                required
+              />
+            </div>
+             <button type="submit"   className="btn btn-success w-100">Send Application</button>
+ </form>
+ </div>
+</div>
+</div>
+</div>
+
+ )
+
+ }
     </div>
     );
 }
